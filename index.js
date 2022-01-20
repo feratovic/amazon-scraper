1d3be540905a17f81f3497f5f6aa47688e1ddee7
Elmaz Feratovic <elmazferatovic16@gmail.com>
2022-01-20 20:43:37 +0100

    first push

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//const apiKey = '80a340bda342df7d8b25b13598bcbc8a';
+const generateScraperUrl = (apiKey) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
+
+//Get Product Details
+app.get('/products/:productId', async (req, res) => {
+  const {productId} = req.params;
+  const {api_key} = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/dp/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Get Product Reviews
+app.get('/products/:productId/reviews', async (req, res) => {
+  const {productId} = req.params;
+  const {api_key} = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Get Product Offers
+app.get('/products/:productId/offers', async (req, res) => {
+  const {productId} = req.params;
+  const {api_key} = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+//Get Search Results
+app.get('/search/:searchQuery', async (req, res) => {
+  const {searchQuery} = req.params;
+  const {api_key} = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key
+      )}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
